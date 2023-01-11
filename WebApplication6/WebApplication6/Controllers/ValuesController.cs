de6e1e3530498f009770882cebfe1d64e33e2dc2
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2023-01-11 18:55:38 +0530

    Add files via upload

---
+++ WebApplication6/WebApplication6/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+using WebApplication6.StudentData;
+
+namespace WebApplication6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("post")]
+        public async Task<string> Post(Student student)
+        {
+            var obj = new Data();
+           var res =  obj.AddStudent(student);
+            var result =  "";
+            return result;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<int> Get(int id)
+        {
+            var res = 1;
+            return res; 
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+
+        public async Task<bool> Delete(int id)
+        {
+            List<int> ints= new List<int>() { 2, 3, 4};
+            var result = ints.Remove(2);
+            
+            return result;
+        }
+    }
+}
---
+++ WebApplication6/WebApplication6/StudentData/Data.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication6.Models;
+
+namespace WebApplication6.StudentData
+{
+    public class Data
+    {
+        public List<Student> students { get; set; }
+        public bool AddStudent(Student student)
+        {
+            students.Add(student);
+            return true;
+        }
+
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+    }
+}
